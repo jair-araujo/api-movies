35f72c5efd20e0a74219ac661dd56159683c8cf8
Jair Araujo <jair.jay@hotmail.com>
2023-04-10 22:17:50 -0300

    modified(UserController): Added update user method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -25,6 +25,44 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado!');
+    }
+
+    const userWithUpdateEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    );
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso!');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(
+      `
+
+    UPDATE user SET
+    name = ?,
+    email = ?
+    updated_at = DATETIME('now)
+    WHERE id = ?
+    `,
+      [user.name, user.email, id]
+    );
+
+    return response.json();
+  }
 }
 
 module.exports = UsersController;
