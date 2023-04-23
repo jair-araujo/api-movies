b960b5b47f029614568c5b8ff11e4e81e993edea
Jair Araujo <jair.jay@hotmail.com>
2023-04-23 19:34:11 -0300

    feat(NoteController): add Index method to NoteController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -41,6 +41,14 @@ class NotesController {
 
     return response.json();
   }
+
+  async index(request, response) {
+    const { user_id } = request.query;
+
+    const notes = await knex('notes').where({ user_id }).orderBy('name');
+
+    return response.json(notes);
+  }
 }
 
 module.exports = NotesController;
